af95788fa94a18de047325609b719a6136460719
David Lantos <david.lantos@gmail.com>
2021-04-26 23:26:25 +0100

    Init empty validator project

---
+++ app/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.4.31"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    mainClass.set("uk.gov.justice.hmpps.referencedata.AppKt")
+}
