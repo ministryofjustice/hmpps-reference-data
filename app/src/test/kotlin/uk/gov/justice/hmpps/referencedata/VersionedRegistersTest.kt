118173b64093d2b4c6b57212d190d88172517de4
David Lantos <david.lantos@gmail.com>
2021-07-23 18:08:53 +0100

    Update test fixture to use the new locations

--- app/src/test/kotlin/uk/gov/justice/hmpps/referencedata/VersionedRegistersTest.kt
+++ app/src/test/kotlin/uk/gov/justice/hmpps/referencedata/VersionedRegistersTest.kt
@@ -8,7 +8,7 @@ import kotlin.test.assertTrue
 
 class VersionedRegistersTest {
     private val firstRepoCommitWithRegister: String = "b8478787640aebddc15d9b80dd3ea6cde6541b44"
-    private val commitWithTestFixtures: String = "2cd90c5ddcf897ab8ac3173ea2c4fd1aa490888b"
+    private val commitWithTestFixtures: String = "0c2513d4eea6375442b1d8c1f4b03e5e96579d32"
 
     @Test
     fun retrievesPreviousVersionsOfRegisters() {
@@ -29,7 +29,7 @@ class VersionedRegistersTest {
         assertTrue(registers.isNotEmpty())
 
         // this test needs to change if this file is changed or moved elsewhere
-        val regions = registers.find { r -> r.path == "probation-regions-v0.csv" }
+        val regions = registers.find { r -> r.path == "registers/probation-regions-v0.csv" }
         assertNotNull(regions)
 
         val currentContent = regions.currentContent
